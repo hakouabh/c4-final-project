da29a572997165de6b52a51be8c82f14f81eaf3b
abdelhak megherbi <megherbi.abdelhak1@gmail.com>
2022-10-23 10:15:00 +0100

    final commit

---
+++ backend/src/lambda/http/updateTodo.ts
@@ -0,0 +1,44 @@
+import 'source-map-support/register'
+
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import * as middy from 'middy'
+import { cors, httpErrorHandler } from 'middy/middlewares'
+
+import { updateTodo } from '../../helpers/todos'
+import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import { getUserId } from '../utils'
+
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const todoId = event.pathParameters.todoId
+    const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
+
+    
+    const userId = getUserId(event);
+    const todoUpdated = await updateTodo(todoId, userId, updatedTodo);
+    if (!todoUpdated) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({
+          error: 'TodoItem n\'existe pas'
+        })
+      };
+    }
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({})
+    }
+  })
+
+handler
+  .use(httpErrorHandler())
+  .use(
+    cors({
+      credentials: true
+    })
+  )
---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '8lleqlhhkg'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-j6okrw88.us.auth0.com',            // Auth0 domain
+  clientId: '9WwCbWGECktTpqcom22On8HVjyEY8IfD',          // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
