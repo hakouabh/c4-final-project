490cdc5dfc86e44db3bccce49c5ac4e97cafa60f
abdelhak megherbi <megherbi.abdelhak1@gmail.com>
2022-10-23 16:42:53 +0100

    le code est divisé en plusieurs fichiers/classes validation de requête

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -4,7 +4,7 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
 
-import { generateUploadUrl } from '../../helpers/todos'
+import { generateUploadUrl } from '../../businessLogic/todos'
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
